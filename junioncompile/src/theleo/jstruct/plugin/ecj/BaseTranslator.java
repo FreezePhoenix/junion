a27a2bed81a4903d99e1534a79780f6f6e5eb3d0
FreezePhoenix <24441367+FreezePhoenix@users.noreply.github.com>
2022-02-01 13:15:39 -0500

    Update BaseTranslator.java

--- junioncompile/src/theleo/jstruct/plugin/ecj/BaseTranslator.java
+++ junioncompile/src/theleo/jstruct/plugin/ecj/BaseTranslator.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2019, Juraj Papp
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  *     * Neither the name of the copyright holder nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -26,61 +26,19 @@
  */
 package theleo.jstruct.plugin.ecj;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import jdk.nashorn.internal.ir.debug.ASTWriter;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.ITypeRoot;
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTMatcher;
-import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.ASTRequestor;
-import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.Assignment;
-import org.eclipse.jdt.core.dom.BooleanLiteral;
-import org.eclipse.jdt.core.dom.CastExpression;
-import org.eclipse.jdt.core.dom.ChildListPropertyDescriptor;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.Expression;
-import org.eclipse.jdt.core.dom.FieldAccess;
-import org.eclipse.jdt.core.dom.ITypeBinding;
-import org.eclipse.jdt.core.dom.InfixExpression;
-import org.eclipse.jdt.core.dom.MethodInvocation;
-import org.eclipse.jdt.core.dom.Name;
-import org.eclipse.jdt.core.dom.NumberLiteral;
-import org.eclipse.jdt.core.dom.ParameterizedType;
-import org.eclipse.jdt.core.dom.ParenthesizedExpression;
-import org.eclipse.jdt.core.dom.PrimitiveType;
-import org.eclipse.jdt.core.dom.QualifiedName;
-import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.SimpleType;
-import org.eclipse.jdt.core.dom.StringLiteral;
-import org.eclipse.jdt.core.dom.StructuralPropertyDescriptor;
-import org.eclipse.jdt.core.dom.Type;
-import org.eclipse.jdt.core.dom.TypeDeclaration;
-import org.eclipse.jdt.core.dom.TypeLiteral;
-import org.eclipse.jdt.core.dom.WildcardType;
-import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
-import org.eclipse.jdt.internal.core.manipulation.dom.ASTResolving;
-import org.eclipse.jdt.internal.corext.dom.ASTNodeFactory;
-import org.eclipse.jdt.internal.corext.dom.ASTNodes;
-import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IRegion;
-import theleo.jstruct.plugin.Log;
 import theleo.jstruct.plugin.ecj.StructCache.Entry;
 import theleo.jstruct.plugin.ecj.StructCache.FieldType;
 import theleo.jstruct.plugin.ecj.StructCache.TypeSymbol;
-import theleo.jstruct.plugin.ecj.Translator.MethodFrame;
-import static theleo.jstruct.plugin.ecj.Translator.R1X;
-import static theleo.jstruct.plugin.ecj.Translator.STRUCT_TYPE_VAR;
 import theleo.jstruct.plugin.ecj.Translator.StringCache.MethodType;
 import theleo.jstruct.plugin.ecj.Translator.StringCache.OpSymbol;
 import theleo.jstruct.plugin.ecj.Translator.TmpTranslator;
+
+import java.util.List;
+
+import static theleo.jstruct.plugin.ecj.Translator.STRUCT_TYPE_VAR;
 import static theleo.jstruct.plugin.ecj.Translator.err;
 
 /**
@@ -94,7 +52,7 @@ public class BaseTranslator extends ASTVisitor {
 	public static final String[] MEM0_B = {"theleo", "jstruct", "hidden", "Mem0", "B"};
 
 	public static final String TYPEBIND_PROP = "theleo.jstruct.CUSTOM_TYPEBIND";
-	
+
 	public static final String TYPEBIND_METHOD_TMP = "theleo.jstruct.METHOD_TMP";
 	public static final String IS_REPLACE = "theleo.jstruct.IS_REPLACE";
 	public static enum TypebindMethodTmp {
@@ -127,18 +85,18 @@ public class BaseTranslator extends ASTVisitor {
 			}
 			n = tmpTranslator.translate(n);
 		}
-		
+
 		ASTNode copy = ASTNode.copySubtree(ast, n);
 		Object t = n.getProperty(TYPEBIND_PROP);
 		if(t != null) copy.setProperty(TYPEBIND_PROP, t);
 		if(m != null) copy.setProperty(TYPEBIND_METHOD_TMP, m);
-		
+
 //		Object t2 = n.getProperty(TYPEBIND_METHOD_TMP);
 //		if(t2 != null) copy.setProperty(TYPEBIND_METHOD_TMP, t2);
 		return copy;
 	}
 	public void replace(ASTNode old, ASTNode neww) {
-		
+
 //		if(!copyStack.isEmpty()) {
 //			if(copyStack.get(copyStack.size()-1) == old) {
 //				copyStack.set(copyStack.size()-1, neww);
@@ -162,33 +120,33 @@ public class BaseTranslator extends ASTVisitor {
 		if(desc instanceof ChildListPropertyDescriptor) {
 			ChildListPropertyDescriptor ch = (ChildListPropertyDescriptor)desc;
 			List<ASTNode> list = (List)parent.getStructuralProperty(ch);
-			
+
 			int index = list.indexOf(old);
-			list.set(index, neww);			
+			list.set(index, neww);
 		}
 		else {
-			if(parent instanceof QualifiedName && 
+			if(parent instanceof QualifiedName &&
 					QualifiedName.QUALIFIER_PROPERTY.getId().equals(desc.getId()) &&
 				!(neww instanceof SimpleName)) {
 				if(!(neww instanceof Expression))throw new IllegalArgumentException();
 				//QualifiedName has to be changed to FieldAccess
-				
+
 //				throw new IllegalArgumentException("qual name expression");
 				FieldAccess fa = ast.newFieldAccess();
 				fa.setExpression((Expression)neww);
 				fa.setName((SimpleName)copySubtreeIfHasParent(((QualifiedName)parent).getName()));
-				
+
 //				for(Map.Entry ee : (Set<Map.Entry>)parent.properties().entrySet()) {
 //					Log.err("ee " + ee.getKey());
 //				}
-				if(parent.getProperty(TYPEBIND_PROP) == null) 
+				if(parent.getProperty(TYPEBIND_PROP) == null)
 					fa.setProperty(TYPEBIND_PROP, parent.getProperty(TYPEBIND_PROP));
-					
+
 				replace(parent, fa);
 				return;
 			}
 			parent.setStructuralProperty(desc, neww);
-		}		
+		}
 	}
 //	ArrayList<ASTNode> copyStack = new ArrayList<>();
 	public ASTNode copyArg(Object o) {
@@ -201,9 +159,9 @@ public class BaseTranslator extends ASTVisitor {
 		ASTNode n = getReplace(node);
 		if(n != node) {
 			err("Different");
-			if(n.getProperty(TYPEBIND_PROP) == null) 
+			if(n.getProperty(TYPEBIND_PROP) == null)
 				throw new CompilerError("Differnet null type: " + node);
-			
+
 			return n;
 			//throw new CompilerError("different");
 		}
@@ -216,9 +174,9 @@ public class BaseTranslator extends ASTVisitor {
 		ASTNode n = getReplace(node);
 		if(n != node) {
 			err("Different");
-			if(n.getProperty(TYPEBIND_PROP) == null) 
+			if(n.getProperty(TYPEBIND_PROP) == null)
 				throw new CompilerError("Differnet null type");
-			
+
 			return n;
 			//throw new CompilerError("different");
 		}
@@ -277,7 +235,7 @@ public class BaseTranslator extends ASTVisitor {
 		if(o instanceof StructCache.FieldType) return null;
 		Entry e = (Entry)o;
 		if(e != null) return e;
-		
+
 		return StructCache.get(name);
 	}
 	public Entry typeLiteral(Object o) {
@@ -413,7 +371,7 @@ public class BaseTranslator extends ASTVisitor {
 		}
 		args.add((Expression)copySubtreeIfHasParent(ArrayIdentifier));
 		args.add(returnInt(0));
-		
+
 		if(dims.size() == 1)
 			args.add(copySubtreeIfHasParent(dims.get(0)));
 		else {
@@ -424,7 +382,7 @@ public class BaseTranslator extends ASTVisitor {
 			for(int i = 0; i < dims.size(); i++) {
 				idx2.arguments().add(copySubtreeIfHasParent(dims.get(i)));
 			}
-			
+
 			for(int i = 0; i < dims.size(); i++) {
 				ASTNode arg3 = (ASTNode)dims.get(i);
 				MethodTmp tmp3 = getMethodTmp(arg3);
@@ -434,7 +392,7 @@ public class BaseTranslator extends ASTVisitor {
 			}
 			args.add(idx2);
 		}
-		
+
 		m.setProperty(TYPEBIND_PROP, TypeBind);
 		m.setProperty(TYPEBIND_METHOD_TMP, new MethodTmp(TypebindMethodTmp.Array, TypeBind));
 		return m;
@@ -446,7 +404,7 @@ public class BaseTranslator extends ASTVisitor {
 		public StructCache.FieldEntry f;
 		public String varData, var;
 //		public boolean isLocalVar;
-		
+
 		public MethodTmp(Entry e) {
 			this.type = TypebindMethodTmp.Ptr;
 			this.e = e;
@@ -471,25 +429,25 @@ public class BaseTranslator extends ASTVisitor {
 			this.varData = varData;
 			this.var = var;
 		}
-		
+
 		public Entry getEntryResult() {
 			if(f == null) return e;
 			if(f.isStruct()) return f.structType;
 			if(f.isReference()) return f.structType;
 			return null;
 		}
-		
-		
+
+
 		public Expression getArrayReference(MethodInvocation mi) {
 			return (Expression)copySubtree(mi.arguments().get(0));
 		}
 		public Expression getLongAddress(MethodInvocation mi) {
 			Expression indexExpr = getLongAddressBase(mi);
-			
+
 			NumberLiteral lit = (NumberLiteral)mi.arguments().get(2);
 			if(offset != Integer.parseInt(lit.getToken()))
 				throw new IllegalArgumentException("offset != " + lit);
-			
+
 			Expression args;
 			if(offset == 0) args = indexExpr;
 			else {
@@ -501,7 +459,7 @@ public class BaseTranslator extends ASTVisitor {
 			}
 			return args;
 		}
-		
+
 		public Expression getLongAddressBase(MethodInvocation mi) {
 			Expression indexExpr = null;
 			if(null != type)
@@ -515,10 +473,10 @@ public class BaseTranslator extends ASTVisitor {
 				case Array_Ptr:
 					MethodInvocation mIndex = ast.newMethodInvocation();
 					mIndex.setExpression((Expression)copySubtree(mi.arguments().get(1)));
-										
+
 					mIndex.setName(name(getIndexMethodName(mi)));
 					fillIndexMethodArgs(mi, mIndex.arguments());
-					
+
 					indexExpr = mIndex;
 //					indexExpr = getLongAddressWithIdx(mIndex);
 					break;
@@ -538,7 +496,7 @@ public class BaseTranslator extends ASTVisitor {
 				store.add(copySubtree(mi.arguments().get(i)));
 			}
 		}
-		public Expression getLongAddressOffset(MethodInvocation mi) {			
+		public Expression getLongAddressOffset(MethodInvocation mi) {
 			return returnInt(offset);
 		}
 		public Expression addLongAddressOffset(Expression e) {
@@ -555,14 +513,14 @@ public class BaseTranslator extends ASTVisitor {
 		}
 //		public Expression getLongAddressWithIdx(MethodInvocation mi) {
 //			return mi;
-////			
+////
 //////			mi.arguments().add(ast.newNumberLiteral(""+e.structSize));
 //////			return mi;
 ////			Expression ret = mi;
-////			
+////
 ////			//test no index checking
 ////			ret = wrap((Expression)copySubtreeIfHasParent(mi.arguments().get(0)));
-////			
+////
 ////			if(e.structSize != 1) {
 ////				InfixExpression mult = ast.newInfixExpression();
 ////				mult.setOperator(InfixExpression.Operator.TIMES);
@@ -571,11 +529,11 @@ public class BaseTranslator extends ASTVisitor {
 ////				return wrap(mult);
 ////			}
 ////			else return ret;
-//		} 
+//		}
 	}
 	public static final String INDEX_METHOD = "idx";
 //	public static final String INDEX_METHOD = "idxCheckOnly";
-	
+
 	public String getIndexMethodName(MethodInvocation mi) {
 		int dims = mi.arguments().size()-3;
 		return getIndexMethodName(dims);
@@ -591,7 +549,7 @@ public class BaseTranslator extends ASTVisitor {
 		else return INDEX_METHOD+'N';
 	}
 
-	
+
 	public void replaceMethodCallArguments(List list) {
 		for(int i = 0; i < list.size(); i++) {
 			Expression expr = (Expression)list.get(i);
@@ -627,9 +585,9 @@ public class BaseTranslator extends ASTVisitor {
 
 		for(Object arg : args)
 			p.arguments().add(arg);
-		
+
 		if(castType != null) {
-			CastExpression cast = ast.newCastExpression();			
+			CastExpression cast = ast.newCastExpression();
 			cast.setType(castType);
 			cast.setExpression(p);
 			cast.setProperty(TYPEBIND_PROP, FieldType.OBJECT);
@@ -637,7 +595,7 @@ public class BaseTranslator extends ASTVisitor {
 		}
 		return p;
 	}
-	
+
 	public void replaceMethodTmp(ASTNode node, MethodInvocation mi, MethodTmp tmp, MethodType type, ASTNode putValue, Assignment.Operator op) {
 		OpSymbol opsymbol = null;
 //					String metName = "put";
@@ -718,7 +676,7 @@ public class BaseTranslator extends ASTVisitor {
 						}
 					}
 					else throw CompilerError.get(getErrorLine(node), CompilerError.UNCATEGORIZED_ERROR);
-					
+
 				}
 				return;
 //				throw new IllegalArgumentException("node " + node + ", " + mi + ", " + type);
@@ -728,16 +686,16 @@ public class BaseTranslator extends ASTVisitor {
 			case Ref:
 			case StackVar:
 //				boolean isLocalVar = tmp.isLocalVar;
-				
-				
+
+
 				FieldType ftype = tmp.f.type;
-				
+
 				TypeSymbol t = tmp.f.type.symbol;
 				if(t == null) {
 //					if(tmp.f.isReference() && tmp.f.isSibling)
 //						t = TypeSymbol.LONG;
 //					else throw new IllegalArgumentException();
-					
+
 					if(tmp.f.isReference()) {
 						if(type == MethodType.get) {
 							throw CompilerError.get(getErrorLine(node), CompilerError.STRUCT_CONVERT_TO_OBJECT);
@@ -766,7 +724,7 @@ public class BaseTranslator extends ASTVisitor {
 									p.arguments().add(returnInt(tmp.f.refData.objOffset));
 								}
 							}
-							
+
 							if(putValue instanceof SimpleName) {
 								SimpleName s = (SimpleName)putValue;
 								p.arguments().add(name(s.getIdentifier()+STRUCT_DATA));
@@ -789,10 +747,10 @@ public class BaseTranslator extends ASTVisitor {
 						if(type == MethodType.get) {
 							throw new IllegalArgumentException();
 						}
-						
+
 						Expression right = (Expression)putValue;
 						MethodTmp tmpL = tmp;
-						
+
 						Entry eL = tmpL.getEntryResult();
 						Entry eR = entry(right);
 						if(eL != null && eR != null) {
@@ -844,7 +802,7 @@ public class BaseTranslator extends ASTVisitor {
 					}
 					else throw new IllegalArgumentException();
 				}
-				
+
 				p.setName(name(Translator.StringCache.getString(
 						type,
 						t,
@@ -852,7 +810,7 @@ public class BaseTranslator extends ASTVisitor {
 
 				p.arguments().add(tmp.getArrayReference(mi));
 				p.arguments().add(tmp.getLongAddress(mi));
-				
+
 
 /*
 				if(tmp.type == TypebindMethodTmp.Ptr) {
@@ -866,7 +824,7 @@ public class BaseTranslator extends ASTVisitor {
 //						p.arguments().add(copySubtreeIfHasParent(arg0));
 //						indexExpr = (Expression)copySubtreeIfHasParent(mi.arguments().get(1));
 //					}
-//					
+//
 					p.arguments().add(copySubtreeIfHasParent(arg0));
 					indexExpr = (Expression)copySubtreeIfHasParent(mi.arguments().get(1));
 //						throw new IllegalArgumentException("not yet implemented");
@@ -890,12 +848,12 @@ public class BaseTranslator extends ASTVisitor {
 				*/
 
 
-				if(ftype == StructCache.FieldType.OBJECT || ftype == StructCache.FieldType.REFERENCE) 
+				if(ftype == StructCache.FieldType.OBJECT || ftype == StructCache.FieldType.REFERENCE)
 					p.arguments().add(returnInt(tmp.f.objOffset));
 
 				if(type == MethodType.put) {
 					if(ftype.primCast && (op != null || (trimWrap((Expression)putValue)) instanceof NumberLiteral)) {
-						CastExpression cast = ast.newCastExpression();			
+						CastExpression cast = ast.newCastExpression();
 						cast.setType(newType(tmp.f.typeb));
 						cast.setExpression((Expression)copySubtree(putValue));
 						cast.setProperty(TYPEBIND_PROP, ftype);
@@ -905,17 +863,17 @@ public class BaseTranslator extends ASTVisitor {
 				}
 
 				if(ftype == StructCache.FieldType.OBJECT) {
-					CastExpression cast = ast.newCastExpression();			
+					CastExpression cast = ast.newCastExpression();
 					cast.setType(newType(tmp.f.typeb));
 					cast.setExpression(p);
 					cast.setProperty(TYPEBIND_PROP, ftype);
 					replace(node, wrap(cast));
-				}			
+				}
 				else replace(node, p);
 
 				break;
 		}
-			
+
 	}
 	public FieldAccess newEntryType(Entry e) {
 		FieldAccess structType = ast.newFieldAccess();
@@ -949,25 +907,25 @@ public class BaseTranslator extends ASTVisitor {
 			e = ((ParenthesizedExpression)e).getExpression();
 		}
 		return e;
-	} 
-	
+	}
+
 	public String getErrorLine(ASTNode node) {
 		int pos = getStartPos(node);
 		if(pos < 0) return null;
 		int line = cu.getLineNumber(pos);
 		try {
 			IRegion r = doc.getLineInformationOfOffset(pos);
-			
+
 //			int len = doc.getLineLength(line);
 //			for(int i = 190; i < 210; i++) {
 //				IRegion r = doc.getLineInformation(i);
 //				Log.err("Line " + i + ": " + r);
 //				Log.err(" OFF " + r.getOffset() + ", len " + r.getLength());
 //				Log.err("xx " + doc.get().substring(r.getOffset(), r.getOffset()+r.getLength()));
-//				
+//
 //			}
 //			Log.err("DOC  " + doc.get());
-//			
+//
 //			Log.err("len fgg " + doc.getLineInformation(line).toString());
 			String text = doc.get(r.getOffset(), r.getLength());
 			return sourceFileName + ":" + line + ": " + text;
